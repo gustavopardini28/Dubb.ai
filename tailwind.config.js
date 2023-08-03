8b9f5ec73be9db11ecf8933ad98508790e1b108e
gustavopardini28 <gustavoemp28@gmail.com>
2023-08-02 22:40:16 -0300

    fisrt commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'gray-100': '#E1E1E6',
+        'gray-300': '#C4C4CC',
+        'gray-400': '#8D8D99',
+        'gray-500': '#7C7C8A',
+        'gray-600': '#323238',
+        'gray-700': '#29292E',
+        'gray-800': '#202024',
+        'gray-900': '#121214',
+      }
     },
   },
-  plugins: [],
+  plugins: [
+
+  ],
 }
